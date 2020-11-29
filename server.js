c6ff887ede8e533454bd37d488e60a5775364edb
Michael <tironamichael@gmail.com>
2020-11-29 11:33:31 -0800

    set up server file

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(require('./routes'))
+
+require('./connection')
+    .sync()
+    .then(() => app.listen(process.env.PORT || 3000, () => console.log('http://localhost:3000')))
+    .catch(err => console.error(err))
