086bb7046c3424e1c013665dbf98492c17c835ac
Michael <tironamichael@gmail.com>
2020-12-03 15:44:50 -0800

    Fixed a typo in the connection file

--- connection/index.js
+++ connection/index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2')
 const Sequelize = require('sequelize')
 
-const sequalize = new Sequelize(process.env.LOCAL_URL)
+const sequelize = new Sequelize(process.env.LOCAL_URL)
 
 module.exports = sequelize
