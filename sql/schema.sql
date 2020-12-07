5adadd2e6dbd99c4db3449c1832e0c9c991f1c36
Michael <tironamichael@gmail.com>
2020-12-07 14:31:52 -0800

    Added sql folder and files

---
+++ sql/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS gift_db;
+
+CREATE DATABASE gift_db;
+
+USE gift_db;
+
+CREATE TABLE lists (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE items (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    price INT NOT NULL,
+    description VARCHAR(30),
+    link VARCHAR(30),
+    FOREIGN KEY listid REFERENCES lists(id)
+);
