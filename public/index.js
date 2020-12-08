e0edfa0b1e029fff40e76d143da25dbd9de1ce9c
Michael <tironamichael@gmail.com>
2020-12-08 15:07:23 -0800

    appending items works

--- public/index.js
+++ public/index.js
@@ -1,1 +1,24 @@
-document.getElementById('')
+document.getElementById('addItem').addEventListener('click', event => {
+    event.preventDefault()
+    axios.post('/api/items', {
+        name: document.getElementById('name').value,
+        price: document.getElementById('price').value,
+        description: document.getElementById('description').value,
+        link: document.getElementById('link').value
+    })
+        .then(() => {
+            let itemElem = document.createElement('div')
+            itemElem.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">${document.getElementById('name').value}</div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">$ ${document.getElementById('price').value}</li>
+                        <li class="list-group-item">${document.getElementById('description').value}</li>
+                        <li class="list-group-item">${document.getElementById('link').value}</li>
+                    </ul>
+                </div>
+            `
+            document.getElementById('gifts').append(itemElem)
+        })
+        // .catch(err => console.error(err))
+})
