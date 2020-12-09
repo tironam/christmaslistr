988e2096d31696d10304422b2bc5b8119da62925
Michael <tironamichael@gmail.com>
2020-12-09 13:04:19 -0800

    Displays items from the DB! It works!

--- public/index.js
+++ public/index.js
@@ -1,3 +1,27 @@
+const getList = () => {
+    axios.get('/api/items')
+        .then(({ data }) => {
+            data.forEach(item => {
+                let itemElem = document.createElement('div')
+                itemElem.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">${item.name}</div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">$${item.price}</li>
+                        <li class="list-group-item">Description: ${item.description}</li>
+                        <li class="list-group-item">Where to buy: ${item.link}</li>
+                    </ul>
+                </div>
+                `
+                document.getElementById('gifts').append(itemElem)
+            })
+        })
+        .catch(err => console.error(err))
+}
+
+getList()
+
+
 document.getElementById('addItem').addEventListener('click', event => {
     event.preventDefault()
     axios.post('/api/items', {
@@ -12,9 +36,9 @@ document.getElementById('addItem').addEventListener('click', event => {
                 <div class="card" style="width: 18rem;">
                     <div class="card-header">${document.getElementById('name').value}</div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">$ ${document.getElementById('price').value}</li>
-                        <li class="list-group-item">${document.getElementById('description').value}</li>
-                        <li class="list-group-item">${document.getElementById('link').value}</li>
+                        <li class="list-group-item">$${document.getElementById('price').value}</li>
+                        <li class="list-group-item">Description: ${document.getElementById('description').value}</li>
+                        <li class="list-group-item">Where to buy: ${document.getElementById('link').value}</li>
                     </ul>
                 </div>
             `
