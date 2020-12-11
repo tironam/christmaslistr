b15de706aaa7af5038477762dbac3c197fe19d64
Michael <tironamichael@gmail.com>
2020-12-11 14:10:02 -0800

    Added jawsdb

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.use(express.json())
 
 app.use(require('./routes'))
 
-app.listen(3000, () => console.log(' Running! http://localhost:3000'))
+app.listen(process.env.PORT || 3000, () => console.log('http://localhost:3000'))
