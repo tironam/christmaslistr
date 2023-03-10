3008bafaa3454acf4ce322f64e960df93116e2ad
Michael Tirona <tironamichael@gmail.com>
2023-03-10 11:34:50 -0500

    Added edit route

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -3,26 +3,36 @@ const db = require('../db')
 
 // GET items based on list
 router.get('/items', (req, res) => {
-    db.query('SELECT * FROM items', (err, items) => {
-        if (err) { console.log(err) }
-        res.json(items)
-    })
+  db.query('SELECT * FROM items', (err, items) => {
+    if (err) {
+      console.log(err)
+    }
+    res.json(items)
+  })
 })
 
 // ADD an item
 router.post('/items', (req, res) => {
-    db.query('INSERT INTO items SET ?', req.body, (err, items) => {
-        if (err) { console.log(err) }
-        res.json(items)
-    })
+  db.query('INSERT INTO items SET ?', req.body, (err, items) => {
+    if (err) {
+      console.log(err)
+    }
+    res.json(items)
+  })
 })
 
 // DELETE an item
-// router.delete('/items/:id', (req, res) => {
-//     db.query(`DELETE FROM items WHERE items.id = ?`, [req.body.id], (err, items) => {
-//         if (err) { console.log(err) }
-//         res.json(items)
-//     })
-// })
+router.delete('/items/:id', (req, res) => {
+  db.query(
+    `DELETE FROM items WHERE items.id = ?`,
+    [req.params.id],
+    (err, items) => {
+      if (err) {
+        console.log(err)
+      }
+      res.json(items)
+    },
+  )
+})
 
 module.exports = router
